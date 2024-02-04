b493b0648cd921af75e731e753b3917ed9e48da3
Max <104013313+Nuyoahhjh@users.noreply.github.com>
2024-02-04 17:36:53 +0800

    Update index.js

--- node_api/db/index.js
+++ node_api/db/index.js
@@ -4,11 +4,11 @@ const mysql = require('mysql')
 
 // 创建连接池
 const pool = mysql.createPool({
-  host: 'cd-cdb-k1lpjubm.sql.tencentcdb.com',
-  port:63983,
-  user: 'root',
-  password: 'Hejunhao01',
-  database: 'tiaozao',
+  host: '******',
+  port:3213,
+  user: '***',
+  password: '*****',
+  database: '****',
   connectionLimit: 20
 })
 
